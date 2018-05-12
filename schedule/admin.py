37ad740ed7e6d25710a9d1aa08a6e73b3ed77d5c
Jason Valladares <jasonvallada@umass.edu>
2018-05-12 17:16:50 -0400

    Admin site now has all data

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Gened, Department, Course, Schedule, Student, ScheduleCourse, Term, Section
 
 # Register your models here.
+
+admin.site.register(Gened)
+admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(Schedule)
+admin.site.register(ScheduleCourse)
+admin.site.register(Term)
+admin.site.register(Section)
