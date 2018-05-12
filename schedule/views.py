bb1942aed3ab24a7450411dee0030d899b6aea5b
Jason Valladares <jasonvallada@umass.edu>
2018-05-12 15:58:06 -0400

    Added uplanner index page

--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    highlight_index = True
+    
+    return render(
+        request,
+        'index.html',
+        context={'highlight_index':highlight_index}
+    )
